9c09e1fc40a9c1c64f5a4698cf4e4e06778a47bb
Nicolas Lopez <nicolaslopez1995@hotmail.com>
2026-01-29 17:10:26 +0000

    logo y favicon1

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,6 +29,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "FlowdeX",
   description: "FlowdeX",
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({
